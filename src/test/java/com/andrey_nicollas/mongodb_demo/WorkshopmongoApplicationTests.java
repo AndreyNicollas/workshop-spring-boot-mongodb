a5f846f6b508684f2bf3b362748c418b6509eaf6
Andrey Nicollas <adsandr3y24@gmail.com>
2025-07-11 17:21:35 -0300

    Projeto criado

---
+++ src/test/java/com/andrey_nicollas/mongodb_demo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrey_nicollas.mongodb_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
