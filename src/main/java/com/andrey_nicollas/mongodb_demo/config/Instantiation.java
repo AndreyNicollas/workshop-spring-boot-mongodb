e27f5c65d884ddc7d425da9947e65e2f490f25c4
Andrey Nicollas <adsandr3y24@gmail.com>
2025-07-13 14:37:12 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/andrey_nicollas/mongodb_demo/config/Instantiation.java
+++ src/main/java/com/andrey_nicollas/mongodb_demo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.andrey_nicollas.mongodb_demo.config;
 
 import com.andrey_nicollas.mongodb_demo.domain.Post;
 import com.andrey_nicollas.mongodb_demo.domain.User;
+import com.andrey_nicollas.mongodb_demo.dto.AuthorDTO;
 import com.andrey_nicollas.mongodb_demo.repository.PostRepository;
 import com.andrey_nicollas.mongodb_demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,11 +35,16 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        User andrey = new User(null, "Andrey Nicollas Costa de Sousa", "andrey974@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem.", "Vou viajar para São Luís. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", maria);
+        // salavando primeiro os usuários no banco de dados,
+        // depois fazer a cópia para o AuthorDTO com os comentários aparecendo com a interação no id;
+        userRepository.saveAll(Arrays.asList(maria, alex, bob, andrey));
 
-        userRepository.saveAll(Arrays.asList(maria, alex, bob));
-        postRepository.saveAll(Arrays.asList(post1, post2));
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem.", "Vou viajar para São Luís. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+        Post post3 = new Post(null, sdf.parse("13/07/2025"), "Bom tarde!", "Nesse momento estou estudando SpringBoot com Java.", new AuthorDTO(andrey));
+
+        postRepository.saveAll(Arrays.asList(post1, post2, post3));
     }
 }
